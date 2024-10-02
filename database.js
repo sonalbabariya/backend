d8d9eb63bc6def6ceafa1981188ebc7736ba0d35
sonalbabariya <sonalbabariya310@gmail.com>
2024-10-02 18:02:48 +0530

    changes

--- database.js
+++ database.js
@@ -1,12 +1,1 @@
-export const arr =[
-  {
-    "username": "sonal kaushik dhaduk",
-    "email": "sonaldhaduk@gmail.com",
-    "id": 1727280140084
-  },
-  {
-    "username": "Nayan babariya",
-    "email": "mansi@gmail.com",
-    "id": 1727280152190
-  }
-]
+export const arr =[]
